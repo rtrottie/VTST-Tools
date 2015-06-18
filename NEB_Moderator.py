e44326bb5fea81cdacd61958b33d9e172c9ad648
ryan <themeisme@gmail.com>
2015-06-18 17:03:08 -0600

    Wrote basic NEB_Moderator with default handlers

--- NEB_Moderator.py
+++ NEB_Moderator.py
@@ -1,63 +1,56 @@
 #!/usr/bin/env python
-#
-# submits job to queue
-#
-# verbose output in qperr
-#
-#
 
-script="$4.sh"
-let nnodes=$1
-let numpernode=$2
-let time=$3
-let nprocs=$numpernode*$nnodes
-outfile=$4
+import sys
+import os
+from custodian.vasp.jobs import *
+from custodian.vasp.handlers import *
 
-# determine which que to go into
+digits = 3
+prefix = 'rep_'
+runtime = 8
 
-que="normal"
+nodes_per_image = sys.argv[1]
+jobname = sys.argv[2]
+logname = jobname + '.log'
+script = jobname + '.log.sh'
 
-if [ $3 -le 1 ]
-  then que="janus-debug"
-fi
+# Determine which folder to run from
 
-if [ $3 -gt 24 ]
- then que="janus-long"
-fi
+def find_last_dir(accum_value,x):
+    if len(x) != (len(prefix)+digits):
+        return accum_value
+    elif x[:len(prefix)] != prefix:
+        return accum_value
+    elif int(x[len(prefix):len(prefix)+digits]) < accum_value[1]:
+        return accum_value
+    else:
+        return (x[len(prefix):len(prefix)+digits],int(x[len(prefix):len(prefix)+digits]))
 
+current_dir_tup = reduce(find_last_dir, next(os.walk('.'))[1],('',-1))
+current_dir = prefix + current_dir_tup[0]
 
-echo $que
+os.chdir(os.path.join(os.path.abspath(os.curdir), current_dir))
+print os.path.abspath('.')
 
+print current_dir
 
+with open(script,'w+') as f:
+    f.write(
+"""#!/bin/bash"
+#SBATCH -J """ + jobname + """
+#SBATCH --time= + """ + runtime + """:00:00
+#SBATCH -N """ + str(nodes_per_image) + """
+#SBATCH --ntasks-per-node 12
+#SBATCH -o """ + logname + """-%j.out
+#SBATCH -e """ + logname + """-%j.err
+#SBATCH --qos=normal
 
-# Header for script adds nodes list from PBS
-echo "#!/bin/bash" > $script
-echo "#SBATCH -J $4
-#SBATCH --time=$3:00:00
-#SBATCH -N $1
-#SBATCH --ntasks-per-node $2
-#SBATCH -o $4-%j.out
-#SBATCH -e $4-%j.err
-#SBATCH --qos=$que " >> $script
+module load intel/intel-12.1.6
+module load openmpi/openmpi-1.4.5_intel-12.1.6_ib
+module load fftw/fftw-3.3.3_openmpi-1.4.5_intel-12.1.0_double_ib
 
-
-echo "module load intel/intel-12.1.6" >> $script
-echo "module load openmpi/openmpi-1.4.5_intel-12.1.6_ib" >> $script  
-echo "module load fftw/fftw-3.3.3_openmpi-1.4.5_intel-12.1.0_double_ib" >> $script
+mpirun -np """ + nodes_per_image + """ /export/home/tester/VASP/vasp.5.3/vasp -d > """ + logname + """
+exit 0""")
  
-
-######## run ###########
-echo 'cd $PBS_O_WORKDIR' >> $script
-
-echo "export OMP_NUM_THREADS=1" >> $script
-
-echo "mpirun -np $nprocs /projects/musgravc/apps/red_hat6/vasp5.3.3/gamma/vasp.5.3/vasp -t /lustre/janus_scratch/$USER -d > $outfile " >> $script
-
-
-
-
-echo "exit 0" >> $script
-
-# Submit script to queue
-sbatch  $script
-
+vaspjob = VaspJob(['sbatch',script],script,auto_gamma=False)
+handlers = WalltimeHandler(runtime*60*60,15*60)
