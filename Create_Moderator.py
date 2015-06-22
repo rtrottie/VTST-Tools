b1f53e3080ac6a563df7ffe473fcd95c9baf2c5c
rtrottie <themeisme@gmail.com>
2015-06-22 10:23:11 -0600

    Fixed bugs related to loading command line variables as strings isntead of int

--- Create_Moderator.py
+++ Create_Moderator.py
@@ -1,33 +1,33 @@
+#!/usr/bin/env python
 from jinja2 import Environment, FileSystemLoader
 from pymatgen.io.vaspio.vasp_input import Incar
 import sys
 import os
 
-os.chdir('/home/ryan/PycharmProjects')
-
-template_location = ('/home/ryan/PycharmProjects/NEB-Tools')
+template_location = ('/home/rytr1806/NEB-Tools')
 template = 'Simple_NEB_Moderator.jinja.py'
 
 env = Environment(loader=FileSystemLoader(template_location))
 template = env.get_template(template)
 
-if len(sys.argv) <= 1:
+if len(sys.argv) < 2:
     sys.argv.append(2)
-if len(sys.argv) <= 2:
+if len(sys.argv) < 3:
     sys.argv.append('NEB_' + os.path.basename(os.getcwd()))
-if len(sys.argv) <= 3:
+if len(sys.argv) < 4:
     sys.argv.append(4)
-nodes_per_image = sys.argv[1]
+nodes_per_image = int(sys.argv[1])
 jobname = sys.argv[2]
-time = sys.argv[3]
+time = int(sys.argv[3])
 incar = Incar.from_file('INCAR')
-images = incar['IMAGES']
+images = int(incar['IMAGES'])
 
 keywords = {'J' : jobname,
             'hours' : time,
             'nodes' : images*nodes_per_image,
             'nntasks_per_node' : 12,
-            'logname' : jobname + '.log'}
+            'logname' : jobname + '.log',
+            'tasks' : images*nodes_per_image*12}
 
 with open(keywords['logname']+'.py', 'w+') as f:
     f.write(template.render(keywords))
