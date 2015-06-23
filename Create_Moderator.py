a1bc6cd5db70f7a75367616eb1d8c932f1a189f8
ryan <themeisme@gmail.com>
2015-06-23 10:23:37 -0600

    Added backup system to script

--- Create_Moderator.py
+++ Create_Moderator.py
@@ -3,12 +3,46 @@ from jinja2 import Environment, FileSystemLoader
 from pymatgen.io.vaspio.vasp_input import Incar
 import sys
 import os
+import shutil
+
+#Backup Previous Run
+os.system('nebmovie.pl; nebbarrier.pl')
+backup_dir = "backup"
+if os.path.isdir(backup_dir):
+    last_run = -1
+    backups = os.listdir(backup_dir)
+    for backup in backups:
+        try:
+            if int(backup) > last_run:
+                last_run = int(backup)
+        except:
+            pass
+    if last_run == -1:
+        raise "backup setup is invalid"
+    this_run = last_run+1
+else:
+    this_run = 0
+
+os.makedirs(os.path.join(backup_dir, str(this_run)))
+
+for dir in os.listdir('.'):
+    if os.path.exists(os.path.join(dir,'CONTCAR')):
+        os.makedirs(os.path.join(backup_dir, str(this_run), dir))
+        shutil.copy(os.path.join(dir,'POSCAR'), os.path.join(backup_dir, str(this_run), dir))
+        shutil.move(os.path.join(dir,'CONTCAR'), os.path.join(dir, 'POSCAR'))
+shutil.copy('INCAR', os.path.join(backup_dir, str(this_run)))
+shutil.copy('movie.xyz', os.path.join(backup_dir, str(this_run)))
+shutil.copy('neb.dat', os.path.join(backup_dir, str(this_run)))
+
+os.system('nebmovie.pl; rm *.out *.err *.sh *.py')
 
 template_location = ('/home/rytr1806/NEB-Tools')
 template = 'Simple_NEB_Moderator.jinja.py'
 
 env = Environment(loader=FileSystemLoader(template_location))
 template = env.get_template(template)
+
+
 
 if len(sys.argv) < 2:
     sys.argv.append(2)
