143e650516033f042b2b26b149570df09d903d11
ryan <themeisme@gmail.com>
2015-07-10 14:55:32 -0600

    fixed debugging

--- Substitute_Atoms.py
+++ Substitute_Atoms.py
@@ -102,8 +102,8 @@ def switch_atom(prev_dir, this_dir, atoms, optional_files=None):
     poscar = Poscar.from_file(os.path.join(prev_dir, 'POSCAR'))
     temp_dir = os.path.join(this_dir, 'temp')
     for i in range(len(atoms)/2):
-        a1 = str(poscar.structure.species[atoms[2*i]])
-        a2 = str(poscar.structure.species[atoms[2*i+1]])
+        a1 = str(poscar.structure.species[atoms[2*i]-1])
+        a2 = str(poscar.structure.species[atoms[2*i+1]-1])
         replace_atom(prev_dir, temp_dir, [atoms[2*i]], a2)
         replace_atom(temp_dir, this_dir, [atoms[2*i+1]], a1)
         os.system('rm -r ' + temp_dir)
