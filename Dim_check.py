e441877a9c702f0aab03c7b96b9f8b78219b3b5e
Ryan (Rapunzel) <themeisme@gmail.com>
2015-08-03 15:34:20 -0600

    added Dimer check function

---
+++ Dim_check.py
@@ -0,0 +1,16 @@
+import os
+import cfg
+import shutil
+from Classes_Pymatgen import *
+
+def check_dimer(directory):
+    os.chdir(directory)
+    os.system(os.path.join(cfg.VTST_DIR, 'dimmins.pl'))
+    for m in ['min1', 'min2']:
+        dir = os.path.join(directory, 'mins', m)
+        shutil.copy(os.path.join(directory, 'WAVECAR'), dir)
+        incar = Incar.from_file(os.path.join(dir,'INCAR'))
+        incar['EDIFFG'] = -5e-2
+        incar.pop('ICHAIN')
+        incar['IOPT'] = 7
+        incar.write_file(os.path.join(dir,'INCAR'))
