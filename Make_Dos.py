1b3bd905f8cae236b184b6f021f6e4384b638195
Ryan (Rapunzel) <themeisme@gmail.com>
2015-12-07 16:53:41 -0700

    easier eg

--- Make_Dos.py
+++ Make_Dos.py
@@ -29,7 +29,7 @@ def get_dos(dos, site, orbital='all'):
         return dos.get_site_dos(dos.structure.sites[site])
     elif orbital == 't2g' or orbital == 'e_g' or orbital == 'eg':
         if orbital == 'eg':
-            orbital == 'e_g'
+            orbital = 'e_g'
         return dos.get_site_t2g_eg_resolved_dos(dos.structure.sites[site])[orbital]
     elif orbital == 's' or orbital == 'p' or orbital == 'd':
         return dos.get_site_spd_dos(dos.structure.sites[site])[orbital.upper()]
