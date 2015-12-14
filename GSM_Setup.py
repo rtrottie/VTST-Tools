a8d67b6282a84d5c01082c85af8e752d79721aff
Ryan-VC <themeisme@gmail.com>
2015-12-14 11:01:21 -0700

    Fixed GSM wrapping issue

--- GSM_Setup.py
+++ GSM_Setup.py
@@ -6,7 +6,25 @@ import Helpers
 import shutil
 import jinja2
 import ase.io
+from ase.utils.geometry import wrap_positions
 from Classes_Pymatgen import *
+
+def wrap_positions_right(positions, center, cell):
+    new_pos = []
+    x=0; y=0; z=0
+    for i in range(len(positions)):
+        if abs(positions[i] - center[i] ) > 0.5:
+            if positions[i] < center[i]:
+                scale = positions[i] + 1
+            else:
+                scale = positions[i] - 1
+        else:
+            scale = positions[i]
+        x += cell[i][0] * scale
+        y += cell[i][1] * scale
+        z += cell[i][2] * scale
+
+    return (x,y,z)
 
 def GSM_Setup():
 
@@ -14,7 +32,10 @@ def GSM_Setup():
     jinja_vars = Helpers.load_variables(os.path.join(file_loc, 'VARS.jinja2'))
 
 
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
+        start = 'start'
+        final = 'final'
+    elif len(sys.argv) == 2:
         jinja_vars['IMAGES'] = sys.argv[1]
         start = 'start'
         final = 'final'
@@ -28,9 +49,14 @@ def GSM_Setup():
 
 
 
-
-    start_pos = os.path.join(start, 'CONTCAR') if os.path.exists(os.path.join(start, 'CONTCAR')) else os.path.join(start, 'POSCAR')
-    final_pos = os.path.join(final, 'CONTCAR') if os.path.exists(os.path.join(final, 'CONTCAR')) else os.path.join(final, 'POSCAR')
+    if not os.path.isfile(start):
+        start_pos = os.path.join(start, 'CONTCAR') if os.path.exists(os.path.join(start, 'CONTCAR')) else os.path.join(start, 'POSCAR')
+    else:
+        start_pos = start
+    if not os.path.isfile(final):
+        final_pos = os.path.join(final, 'CONTCAR') if os.path.exists(os.path.join(final, 'CONTCAR')) else os.path.join(final, 'POSCAR')
+    else:
+        final_pos = final
     images = int(jinja_vars['IMAGES'])
 
 
@@ -39,8 +65,8 @@ def GSM_Setup():
 
     shutil.copy(os.path.join(file_loc, 'gfstringq.exe'), 'gfstringq.exe')
     shutil.copy(os.path.join(file_loc, 'status'), 'status')
-    shutil.copy(start_pos, 'start')
-    shutil.copy(final_pos, 'final')
+    shutil.copy(start_pos, 'POSCAR.start')
+    shutil.copy(final_pos, 'POSCAR.final')
     incar = Incar.from_file(os.path.join(start, 'INCAR'))
     incar['NSW']=0
     incar.write_file('INCAR')
@@ -65,9 +91,15 @@ def GSM_Setup():
     os.chmod('grad.py', 0o755)
     start = ase.io.read(start_pos)
     final = ase.io.read(final_pos)
+    final_positions = []
+    for i in range(len(final)):
+        pos = [final[i].a, final[i].b, final[i].c]
+        center = [start[i].a, start[i].b, start[i].c]
+        final_positions.append(wrap_positions_right(pos, center, start.cell))
+    final.set_positions(final_positions)
     ase.io.write('scratch/initial0000.temp.xyz',[start,final])
     with open('scratch/initial0000.temp.xyz', 'r') as f:
-        sd = Poscar.from_file('start').selective_dynamics
+        sd = Poscar.from_file('POSCAR.start').selective_dynamics
         sd = map(lambda l : '\n' if (l[0] or l[1] or l[2]) else ' "X"\n', sd)
         to_zip = ['\n', '\n'] + sd + ['\n', '\n'] + sd
         zipped = zip(f.readlines(), to_zip)
