82aeee257a75545dbe65a7eca132a1b7f34c0594
Ryan-VC <themeisme@gmail.com>
2016-01-04 12:52:36 -0700

    fixed wrapping issues

--- GSM_Setup.py
+++ GSM_Setup.py
@@ -14,11 +14,13 @@ def wrap_positions_right(positions, center, cell):
     x=0; y=0; z=0
     for i in range(len(positions)):
         scale = positions[i]
-        while abs(center[i] - positions[i]) > 0.5:
+        if abs(center[i] - positions[i]) > 0.5:
             if positions[i] < center[i]:
                 scale = positions[i] + 1
             else:
                 scale = positions[i] - 1
+        else:
+            scale = positions[i]
         x += cell[i][0] * scale
         y += cell[i][1] * scale
         z += cell[i][2] * scale
