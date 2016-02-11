cd833df3b79bf2cb7da1fd238f4770a6a480dd39
Ryan-VC <themeisme@gmail.com>
2016-02-11 11:54:20 -0700

    stop reordering atoms
    begining argparse

--- Vacancy_Correction.py
+++ Vacancy_Correction.py
@@ -3,6 +3,7 @@ from Classes_Pymatgen import *
 from pymatgen.transformations.site_transformations import *
 import os
 import sys
+import argparse
 
 def reorder_vacancies(pos1, pos2, coord1, coord2, writeDir):
     if coord1 < coord2:
@@ -25,12 +26,43 @@ def reorder_vacancies(pos1, pos2, coord1, coord2, writeDir):
     r1 = RemoveSitesTransformation([coord2 - 1])
     r2 = RemoveSitesTransformation([coord1 - 1])
 
-    s1 = ins1.apply_transformation(r1.apply_transformation(p1.structure))
-    s2 = ins2.apply_transformation(r2.apply_transformation(p2.structure))
+    temp_s1 = ins1.apply_transformation(r1.apply_transformation(p1.structure))
+    temp_s2 = ins2.apply_transformation(r2.apply_transformation(p2.structure))
+
+    # Need to sort list to ensure atoms are the same as before
+    # Unfortuantely sort function does not work as intened for this purpose
+    sites_1 = []
+    sites_2 = []
+    site_symbols = p1.site_symbols
+    for symbol in site_symbols:
+        def match_symbol(site):
+            return site.specie.symbol == symbol
+        l_1 = [site for site in temp_s1.sites if match_symbol(site)]
+        l_2 = [site for site in temp_s2.sites if match_symbol(site)]
+        sites_1.extend(l_1)
+        sites_2.extend(l_2)
+    s1 = Structure.from_sites(sites_1)
+    s2 = Structure.from_sites(sites_2)
+
     p1 = Poscar(s1)
     p2 = Poscar(s2)
     p1.write_file(os.path.join(writeDir, 'POSCAR-1'))
     p2.write_file(os.path.join(writeDir, 'POSCAR-2'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('struct_1', help='structure file or VASP run folder for the first structure')
+    parser.add_argument('struct_2', help='structure file or VASP run folder for the second structure')
+    parser.add_argument('atom_1', help='migrating atom in struct_1 (1 indexed by default)')
+    parser.add_argument('atom_2', help='migrating atom in struct_2 (1 indexed by default)')
+    parser.add_argument('-d', '--directory', help='directory to write too')
+
+    if os.path.isfile(start):
+        start_file = start
+        start_folder = os.path.dirname(start)
+    else:
+        start_file = os.path.join(start, 'CONTCAR') if os.path.exists(os.path.join(start, 'CONTCAR')) else os.path.join(start, 'POSCAR')
+        start_folder = start
 
 if os.path.basename(sys.argv[0]) == 'Vacancy_Correction.py':
     if len(sys.argv) < 2:
