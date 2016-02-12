93a8e582556e5616958b9b607a4e75cc86031f64
Ryan-VC <themeisme@gmail.com>
2016-02-12 15:58:42 -0700

    remove logs

--- vasp.py
+++ vasp.py
@@ -29,7 +29,7 @@ def get_instructions_for_backup(jobtype, incar='INCAR'):
 
     '''
     instructions = {}
-    instructions["commands"] = ['rm *.out *.err STOPCAR *.e[0-9][0-9][0-9]* *.o[1-9][1-9][1-9]* &> /dev/null']
+    instructions["commands"] = ['rm *.out *.err STOPCAR *.e[0-9][0-9][0-9]* *.o[1-9][1-9][1-9]* *.log* &> /dev/null']
     instructions['backup'] = []
     instructions['move'] = []
     if jobtype == 'Standard':
