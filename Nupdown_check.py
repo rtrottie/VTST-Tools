4b7deb2308ae535d90cea385581a8cbcd8ade24d
ryan <themeisme@gmail.com>
2016-03-22 15:21:06 -0600

    Nupdown now accepts range

--- Nupdown_check.py
+++ Nupdown_check.py
@@ -7,10 +7,12 @@ import shutil
 from Classes_Pymatgen import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('center', help='magmom to check values from',
+parser.add_argument('start', help='magmom to check values from',
                     type=int, default=0)
-parser.add_argument('radius', help='Number of values to check around (default = 4 (9 jobs)) OPTIONAL',
-                    type=int, default=4, nargs='?')
+parser.add_argument('end', help='magmom to check values to (inclusive)',
+                    type=int, nargs='?')
+parser.add_argument('-r', '--radius', help='Number of values to check around (default = 4 (9 jobs)) Only used if two values aren\'t specified',
+                    type=int, default=4)
 parser.add_argument('-s', '--system', help='Don\'t modify SYSTEM variable in INCAR.  By default NUPDOWN is prepended to this')
 parser.add_argument('-w', '--wavecar', help='Copy WAVECAR file',
                     action='store_true')
@@ -25,7 +27,14 @@ system = incar["SYSTEM"]
 if not os.path.exists('nupdown'):
     os.makedirs('nupdown')
 
-for i in range(args.center - args.radius, args.center + args.radius + 1):
+if args.end:
+    start = args.start if args.start < args.end else args.end
+    end = args.end + 1 if args.start < args.end else args.start + 1
+else:
+    start = args.center - args.radius
+    end = args.center + args.radius + 1
+
+for i in range(start, end):
     dir = os.path.join('nupdown', str(i).zfill(3))
     print('Setting up run in ./' + dir)
     if not os.path.exists(dir):
