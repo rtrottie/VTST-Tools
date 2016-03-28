8ba5a06a33a8e83283bd4a7fff32775bd5a3745d
ryan <themeisme@gmail.com>
2016-03-28 08:59:42 -0600

    making gsm2dim script

--- gsm2dim.py
+++ gsm2dim.py
@@ -2,6 +2,29 @@
 
 from Classes_Pymatgen import *
 import argparse
+import shutil
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument('ts-image', help='Image of TS',
+                    type=int)
+args = parser.parse_args()
+
+ts = 'scratch/IMAGE.' + str(args.ts_image).zfill(2)
+grad1 = '../scratch/IMAGE.' + str(args.ts_image - 1).zfill(2)
+grad2 = '../scratch/IMAGE.' + str(args.ts_image + 1).zfill(2)
+
+print('Copying Old Run')
+
+shutil.copy(ts, 'dim')
+os.chdir('dim')
+
+print('Setting up INCAR')
+incar = Incar.from_file('INCAR')
+incar['ICHAIN'] = 2
+incar['EDIFF'] = 1e-7
+incar['NSW'] = 5000
+incar.write_file('INCAR')
+
+print('Making MODECAR')
+os.system('modemake.pl ' + grad1 + ' ' + grad2)
