17530eb0036543b464b499a8169b9e4a147f5a79
ryan <themeisme@gmail.com>
2016-05-25 08:00:31 -0600

    bandgap

---
+++ band-gap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from pymatgen.io.vasp.outputs import *
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser
+    parser.add_argument('vasprun', help='filename of vasprun.xml file (default: vasprun.xml',
+                        nargs='?', default='vasprun.xml')
+    args = parser.parse_args()
+    v = Vasprun(args.vasprun)
+    bs = v.get_band_structure()
+    print bs.get_band_gap()
