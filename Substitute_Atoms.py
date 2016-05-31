b0fb0b746c999805acf82406312ccd440c0add29
ryan <themeisme@gmail.com>
2016-05-31 16:17:00 -0600

    don't crash if sd doesn't appear

--- Substitute_Atoms.py
+++ Substitute_Atoms.py
@@ -69,9 +69,10 @@ def replace_atom(prev_dir, this_dir, atom_nums, new_atom, optional_files=None):
 
     # Modifying POSCAR
     sd = vasp['POSCAR'].selective_dynamics
-    vasp['POSCAR'].structure = transformation.apply_transformation(vasp['POSCAR'].structure)
-    vasp['POSCAR'].comment = ' '.join(vasp['POSCAR'].site_symbols)
-    vasp['POSCAR'].selective_dynamics = sd
+    if sd:
+        vasp['POSCAR'].structure = transformation.apply_transformation(vasp['POSCAR'].structure)
+        vasp['POSCAR'].comment = ' '.join(vasp['POSCAR'].site_symbols)
+        vasp['POSCAR'].selective_dynamics = sd
 
     # Creating new POTCAR
     vasp['POTCAR'] = Potcar(vasp['POSCAR'].site_symbols)
