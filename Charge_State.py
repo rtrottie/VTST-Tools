ce3342d1a00b81b9eb482094ce91e798c93503d9
ryan <themeisme@gmail.com>
2016-06-17 09:58:39 -0600

    fixed ==

--- Charge_State.py
+++ Charge_State.py
@@ -28,7 +28,7 @@ base_nelect = get_nelect(os.path.join(args.folder, 'OUTCAR'))
 
 for i in range(args.start, args.end+1):
     dir = os.path.join(str(i))
-    if dir[0] = '-':
+    if dir[0] == '-':
         dir[0] = 'n'
     if not os.path.exists(dir):
         print('Setting up run in ./' + dir)
