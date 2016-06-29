bef6f841b0e88550c85b4a896a3927c19fdcd700
ryan <themeisme@gmail.com>
2016-06-29 11:49:48 -0600

    L:

--- chg.py
+++ chg.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', type=str, default='CHGCAR')
 args = parser.parse_args()
 
-c = Chgcar.from_file(args.f)
+c = Chgcar.from_file(args.file)
 for v in range(3):
     with open(args.f.lower() + '.' + str(v) + '.txt', 'w') as f:
         f.writelines([ str(x) + '\n' for x in c.get_average_along_axis(v) ])
