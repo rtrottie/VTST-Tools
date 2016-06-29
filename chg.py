8abaf0894ab942e1d790d7a2a07cb7c8a564cc09
ryan <themeisme@gmail.com>
2016-06-29 11:36:17 -0600

    added chg

---
+++ chg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import argparse
+from pymatgen.io.vasp.outputs import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('vector', type=int)
+args = parser.parse_args()
+
+c = Chgcar.from_file('CHGCAR')
+with open('charge_density.txt') as f:
+    f.writelines([ str(x) + '\n' for x in c.get_average_along_axis(args.vector) ])
