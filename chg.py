a0daf3449dde4b7f53c8db664c529764224fbe88
ryan <themeisme@gmail.com>
2016-06-29 11:49:07 -0600

    more chg options

--- chg.py
+++ chg.py
@@ -5,9 +5,10 @@ from pymatgen.io.vasp.outputs import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('vector', type=int)
+parser.add_argument('-f', '--file', type=str, default='CHGCAR')
 args = parser.parse_args()
 
-c = Chgcar.from_file('CHGCAR')
-with open('charge_density.txt', 'w') as f:
-    f.writelines([ str(x) + '\n' for x in c.get_average_along_axis(args.vector) ])
+c = Chgcar.from_file(args.f)
+for v in range(3)
+    with open(args.f.lower() + '.' + str(v) + '.txt', 'w') as f:
+        f.writelines([ str(x) + '\n' for x in c.get_average_along_axis(v) ])
