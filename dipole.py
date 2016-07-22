f062113a25d707c59865fce7ca3b094fc24546ed
ryan <themeisme@gmail.com>
2016-07-22 12:22:13 -0600

    can do it with atoms now

--- dipole.py
+++ dipole.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import argparse
 import subprocess
+from pymatgen.io.vasp.outputs import Chgcar
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('axis', help='Axis to average along (1,2, or 3)',
                         type=int)
     parser.add_argument('atoms', help='Atoms to add up',
-                        type=int, nargs='*')
+                        type=str, nargs='*')
     args = parser.parse_args()
 
     p = subprocess.Popen(['chgsum.pl', 'AECCAR0', 'AECCAR2'])
     p.wait()
-    p = subprocess.Popen(['bader', 'CHGCAR_sum'])
+    p = subprocess.Popen(['bader', 'CHGCAR_sum', '-p', 'sum_atom'] + args.atoms)
+    p.wait()
+
+    c = Chgcar.from_file()
