c3e8b9908da77251f9804347e184e4f249c19a16
ryan <themeisme@gmail.com>
2016-07-28 11:58:44 -0600

    mod shouldn't do the same thing

--- dipole.py
+++ dipole.py
@@ -76,12 +76,12 @@ if __name__ == '__main__':
     mod_b = int(np.round(float(len_b) * args.origin[1]))
     mod_c = int(np.round(float(len_c) * args.origin[2]))
     def get_first_moment(a, b, c, x):
+        a = float(a) / len_a
+        b = float(b) / len_b
+        c = float(c) / len_c
         a_i = (a + mod_a) % len_a
         b_i = (b + mod_b) % len_b
         c_i = (c + mod_c) % len_c
-        a = float(a_i) / len_a
-        b = float(b_i) / len_b
-        c = float(c_i) / len_c
         cart_vector = np.matrix([a, b, c]) * s.lattice.matrix
         return float(np.dot(cart_vector, unit_vector)) * (x + correction)
 
