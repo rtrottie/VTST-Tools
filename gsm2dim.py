92991b9687d817a50fcafba0339f8b101cf3d73f
ryan <themeisme@gmail.com>
2016-09-15 10:47:12 -0600

    python 2-> 3

--- gsm2dim.py
+++ gsm2dim.py
@@ -15,7 +15,7 @@ if args.ts_image < 0:
     energies = subprocess.check_output(['grep V_profile scratch/paragsm0000 | tail -n 1'], shell=True)
     energies = [ float(x) for x in energies.split()[1:] ]
     args.ts_image = energies.index(max(energies)) + 1
-    print 'TS Image Determined to be:  ' + str(args.ts_image)
+    print('TS Image Determined to be:  ' + str(args.ts_image))
 
 
 ts = 'scratch/IMAGE.' + str(args.ts_image).zfill(2)
