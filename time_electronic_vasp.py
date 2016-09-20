5e01cccc121679a32041e4d6736d196651e0a963
ryan <themeisme@gmail.com>
2016-09-20 12:18:40 -0600

    python 2-> 3

--- time_electronic_vasp.py
+++ time_electronic_vasp.py
@@ -4,7 +4,7 @@ import re
 import numpy
 
 #times = [i for i in subprocess.check_output(['grep', 'LOOP:', 'OUTCAR']).split('\n')]
-time_lines = [s.split() for s in subprocess.check_output(['grep', 'LOOP:', 'OUTCAR']).split('\n')]
+time_lines = [s.split() for s in subprocess.check_output(['grep', 'LOOP:', 'OUTCAR']).decode().split('\n')]
 times = [float(l[6]) for l in time_lines if (len(l) == 7 and re.match("^\d+?\.\d+?$", l[6]) != None)]
 
 if len(times) > 6:
