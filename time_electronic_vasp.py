55e8f5746dfabbae09cfc011d009152478d61f58
ryan <themeisme@gmail.com>
2016-10-24 12:37:16 -0600

    get times from everywhere

--- time_electronic_vasp.py
+++ time_electronic_vasp.py
@@ -2,9 +2,14 @@
 import subprocess
 import re
 import numpy
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('OUTCAR', help='OUTCAR file (default = "OUTCAR")',
+                    default='OUTCAR', nargs='?')
+args = parser.parse_args()
 #times = [i for i in subprocess.check_output(['grep', 'LOOP:', 'OUTCAR']).split('\n')]
-time_lines = [s.split() for s in subprocess.check_output(['grep', 'LOOP:', 'OUTCAR']).decode().split('\n')]
+time_lines = [s.split() for s in subprocess.check_output(['grep', 'LOOP:', args.OUTCAR]).decode().split('\n')]
 times = [float(l[6]) for l in time_lines if (len(l) == 7 and re.match("^\d+?\.\d+?$", l[6]) != None)]
 
 if len(times) > 6:
