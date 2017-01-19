e602ae645a9000c7bb666376e29b86b9a61394be
Ryan Trottier <themeisme@gmail.com>
2017-01-19 11:04:43 -0700

    better nebmake

--- Neb_Make.py
+++ Neb_Make.py
@@ -69,7 +69,7 @@ def nebmake(directory, start, final, images, tolerance=0, ci=False, poscar_overr
             atoms.append( (poscar_override[i*2], poscar_override[i*2+1]))
         (s1, s2) = reorganize_structures(s1, s2, atoms=atoms, autosort_tol=tolerance)
         structures = poscar_override[0].interpolate(poscar_override[1], images, autosort_tol=tolerance)
-    structures = s1.interpolate(s2., images, autosort_tol=tolerance)
+    structures = s1.interpolate(s2, images, autosort_tol=tolerance)
 
     incar['ICHAIN'] = 0
     incar['IMAGES'] = images-1
