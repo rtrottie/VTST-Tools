5d6fee13e31d038752428ee4cb88949745a2d99f
Ryan Trottier <themeisme@gmail.com>
2017-01-19 11:07:09 -0700

    remember to use range

--- Neb_Make.py
+++ Neb_Make.py
@@ -68,7 +68,6 @@ def nebmake(directory, start, final, images, tolerance=0, ci=False, poscar_overr
         for i in range(int(len(poscar_override)/2)):
             atoms.append( (poscar_override[i*2], poscar_override[i*2+1]))
         (s1, s2) = reorganize_structures(s1, s2, atoms=atoms, autosort_tol=tolerance)
-        structures = poscar_override[0].interpolate(poscar_override[1], images, autosort_tol=tolerance)
     structures = s1.interpolate(s2, images, autosort_tol=tolerance)
 
     incar['ICHAIN'] = 0
