8c268744362571c270f099b37b92a239c8a5f8fe
Ryan Trottier <themeisme@gmail.com>
2017-01-19 11:02:32 -0700

    better nebmake

--- Neb_Make.py
+++ Neb_Make.py
@@ -2,11 +2,54 @@
 
 import argparse
 from Classes_Pymatgen import *
+from pymatgen.core import Site
 import os
 import shutil
 
+def reorganize_structures(structure_1 : Structure, structure_2 : Structure, atoms=[], autosort_tol=0.5):
+    '''
 
-def nebmake(directory, start, final, images, tolerance=0, ci=False):
+    :param structure_1_mutable: Structure
+        Structure to interpolate between
+    :param structure_2_mutable: Structure
+        Structure to interpolate between
+    :param atoms:
+        list of tuples of atoms that should be the same between structures
+    :return: Structure
+    '''
+
+    # parse Atoms into two lists
+    atom_i_1 = []
+    atom_i_2 = []
+    atoms_1 = []
+    atoms_2 = []
+    for i_1, i_2 in atoms:
+        atom_i_1.append(i_1)
+        atom_i_2.append(i_2)
+        atoms_1.append(structure_1[i_1])
+        atoms_2.append(structure_2[i_2])
+
+    # Remove sites
+    structure_1_mutable = structure_1.copy() # type: Structure
+    structure_2_mutable = structure_2.copy() # type: Structure
+    structure_1_mutable.remove_sites(atom_i_1)
+    structure_2_mutable.remove_sites(atom_i_2)
+
+    images = structure_1_mutable.interpolate(structure_2_mutable, 1, autosort_tol=autosort_tol)
+    new_s_1 = images[0] # type: Structure
+    new_s_2 = images[1] # type: Structure
+
+    for atom in atoms_1: # type: Site
+        atom = atom
+        new_s_1.append(atom.specie, atom.coords, properties=atom.properties)
+
+    for atom in atoms_2: # type: Site
+        atom = atom
+        new_s_1.append(atom.specie, atom.coords, properties=atom.properties)
+
+    return (new_s_1, new_s_2)
+
+def nebmake(directory, start, final, images, tolerance=0, ci=False, poscar_override=[]):
     start_POSCAR = os.path.join(start, 'CONTCAR') if os.path.exists(os.path.join(start, 'CONTCAR')) and os.path.getsize(os.path.join(start, 'CONTCAR')) > 0 else os.path.join(start, 'POSCAR')
     final_POSCAR = os.path.join(final, 'CONTCAR') if os.path.exists(os.path.join(final, 'CONTCAR')) and os.path.getsize(os.path.join(final, 'CONTCAR')) > 0 else os.path.join(final, 'POSCAR')
 
@@ -18,7 +61,15 @@ def nebmake(directory, start, final, images, tolerance=0, ci=False):
 
     p1 = Poscar.from_file(start_POSCAR)
     p2 = Poscar.from_file(final_POSCAR)
-    structures = p1.structure.interpolate(p2.structure, images, autosort_tol=tolerance)
+    s1 = p1.structure
+    s2 = p2.structure
+    if poscar_override:
+        atoms = []
+        for i in int(len(poscar_override)/2):
+            atoms.append( (poscar_override[i*2], poscar_override[i*2+1]))
+        (s1, s2) = reorganize_structures(s1, s2, atoms=atoms, autosort_tol=tolerance)
+        structures = poscar_override[0].interpolate(poscar_override[1], images, autosort_tol=tolerance)
+    structures = s1.interpolate(s2., images, autosort_tol=tolerance)
 
     incar['ICHAIN'] = 0
     incar['IMAGES'] = images-1
@@ -52,5 +103,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--directory', help='where to create neb (default: ".")',
                         default='.')
     parser.add_argument('-c', '--climbing_image', help='use CI', action = 'store_true')
+    parser.add_argument('-a', '--atom_pairs', help='pair certain atoms', type=int, nargs='*', default=[])
     args = parser.parse_args()
+    if args.atom_pairs:
+        atom_overrides = reorganize_structures()
     nebmake(args.directory, args.initial, args.final, args.images+1, args.tolerance, args.climbing_image)
