f3ccbf01dbc8ae57dc92d83b239a3eb40af0d640
Ryan Trottier <themeisme@gmail.com>
2017-04-20 14:43:52 -0600

    its a python

--- freeze_along_vector.py
+++ freeze_along_vector.py
@@ -9,20 +9,20 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('lower_bound', help='vector to begin freezing at',
                     default=None, type=float)
-parser.add_argument('upper_bound', help='vector to begin freezing at',
+parser.add_argument('upper_bound', help='vector to end freezing at',
                     default=None, type=float)
 parser.add_argument('--vector', help='Vector to Freeze along (default = 2)',
                     default=2, type=int)
-parser.add_argument('--file', help='file to read (default = CONTCAR)',
+parser.add_argument('--input', help='file to read (default = CONTCAR)',
                     default='CONTCAR', type=str)
 parser.add_argument('--output', help='file to write (default = CONTCAR.sd)',
                     default='CONTCAR.sd', type=str)
 args = parser.parse_args()
 
 lb = args.lower_bound
-ub = args.uppper_bound
+ub = args.upper_bound
 v = args.vector
 
-s = Structure.from_file(args.file)
+s = Structure.from_file(args.input)
 sd = get_SD_along_vector(s, v, [lb, ub])
 Poscar.write_file(args.output)
