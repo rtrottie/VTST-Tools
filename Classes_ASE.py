0e754ded8369baebe9400bed67c4db3e8cbc348f
Ryan Trottier <themeisme@gmail.com>
2017-09-27 14:15:53 -0600

    converted to hookean plane

--- Classes_ASE.py
+++ Classes_ASE.py
@@ -69,3 +69,45 @@ class InvertPlane:
         # Get equation of plane ax+by+cz+d = 0
         perp_projection = 2 * np.dot(normal, forces[self.diffusing_i] ) * normal # Invert Forces
         forces[self.diffusing_i] = forces[self.diffusing_i] - perp_projection
+
+class HookeanPlane:
+    def __init__(self, diffusing_i, plane_i, spring=10):
+        self.diffusing_i = diffusing_i
+        self.plane_i = plane_i
+
+    def adjust_positions(self, oldpositions, newpositions):
+        return
+
+    def adjust_forces(self, atoms, forces):
+        # get Normal Vector
+        p1 = atoms.positions[self.plane_i[0]] # type: np.array
+        p2 = atoms.positions[self.plane_i[1]] # type: np.array
+        p3 = atoms.positions[self.plane_i[2]] # type: np.array
+        v1 = p2 - p1
+        v2 = p3 - p1
+        normal = np.cross(v1, v2) / np.linalg.norm(np.cross(v1,v2))
+
+        # Get equation of plane ax+by+cz+d = 0
+        perp_projection = np.dot(normal, forces[self.diffusing_i] ) * normal
+
+        # Update Forces to remove perp_projection
+        forces[self.diffusing_i] = forces[self.diffusing_i] - perp_projection
+
+
+        # Add Hookean Perp_projection
+        # get Normal Vector
+
+        # Get equation of plane ax+by+cz+d = 0
+        normal = np.cross(v1, v2)
+        a = normal[0]
+        b = normal[1]
+        c = normal[2]
+        d = -(a*p1[0] + b*p1[1] + c*p1[2])
+
+        # Get closest point on plane
+        p = atoms.positions[self.diffusing_i]
+        x = - (d + a*p[0] + b*p[1] + c*p[2]) / (a**2 + b**2 + c**2)
+        position = [p[0] + x*a, p[1] + x*b, p[2] + x*c]
+
+        # Add restoring Force
+        forces[self.diffusing_i] -= np.linalg.norm(atoms.positions[self.diffusing_i] - position)
