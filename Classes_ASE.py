b9548f4bacaba6039cf43168f593e96c8e894165
Ryan Trottier <themeisme@gmail.com>
2017-11-08 14:51:38 -0700

    don't rearrange atoms

--- Classes_ASE.py
+++ Classes_ASE.py
@@ -9,10 +9,8 @@ from ase.geometry import wrap_positions
 from ase import Atoms
 
 class StandardVasp(Vasp):
-    def write_input(self, atoms, directory='./'):
-        from ase.io.vasp import write_vasp
-        write_vasp(os.path.join(directory, 'POSCAR'), atoms=atoms, symbol_count = self.symbol_count)
-        self.write_sort_file(directory=directory)
+    def write_input(self, atoms : Atoms, directory='./'):
+        atoms.write(os.path.join(directory, 'POSCAR'))
 
 class InPlane:
     def __init__(self, diffusing_i, plane_i):
