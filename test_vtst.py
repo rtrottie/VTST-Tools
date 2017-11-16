fd9aebf8ca5014c23c3c414146bef395ab5a8314
Ryan Trottier <themeisme@gmail.com>
2017-11-16 11:21:17 -0700

    smaller interpolations

--- test_vtst.py
+++ test_vtst.py
@@ -1,9 +1,11 @@
-from Classes_ASE import InMPPlane
+from Classes_ASE import InMPPlaneXY
 from ase.io import read
+from Vis import view
 
 a=read('D:\\Users\\RyanTrottier\\Documents\\Scrap\\POSCAR')
-ip = InMPPlane(48, [102, 32])
+ip = InMPPlaneXY(48, [102, 32])
 pos = a.get_positions()
 a.set_constraint(ip)
 a.set_positions(pos)
 a.write('D:\\Users\\RyanTrottier\\Documents\\Scrap\\CONTCAR')
+view('D:\\Users\\RyanTrottier\\Documents\\Scrap\\CONTCAR', 'vesta')
