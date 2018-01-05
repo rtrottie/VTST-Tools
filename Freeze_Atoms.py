3c2000b62de5add7e913e7c52106d982fd0ec94d
Ryan Trottier <themeisme@gmail.com>
2018-01-05 14:48:32 -0700

    select input and output

--- Freeze_Atoms.py
+++ Freeze_Atoms.py
@@ -11,24 +11,24 @@ import argparse
 import pymatgen.io.vasp
 
 
-def freeze_atoms_except_neighbors(dir : str, atom : int, invert=False, unfrozen_dist=4):
+def freeze_atoms_except_neighbors(input : str, atom : int, output : str, invert=False, unfrozen_dist=4):
     '''
 
     #!/usr/bin/env python
     Freezes all atoms around a specified atom, by default, 4 angstrom radius (chosen arbitrarily).  Creates file named
     selective_dynamics which saves the previous selective_dynamics to be restored later.  Saves POSCAR to current folder
 
-    :param dir: Directory to freeze atoms in
+    :param input: Directory to freeze atoms in
     :param atom: number of atom in POSCAR of dir
     :param invert: Normally will leave all atoms in radius able to move, setting this to true will freeze all atoms in radius
     :param unfrozen_dist: radius to freeze atom in
     '''
     atom = atom-1
-    poscar = Poscar.from_file(os.path.join(dir, 'POSCAR'))
-    if os.path.exists(os.path.join(dir, 'selective_dynamics')): # if atoms are alread frozen, we don't want to overwrite original SD
+    poscar = Poscar.from_file(input)
+    if os.path.exists(os.path.join(input, 'selective_dynamics')): # if atoms are alread frozen, we don't want to overwrite original SD
         raise Exception('Selective Dynamics File Exists')
     sd_orig = poscar.selective_dynamics if poscar.selective_dynamics != None else [[True, True, True]]*len(poscar.structure)
-    with open(os.path.join(dir, 'selective_dynamics'),'w+') as f:
+    with open(os.path.join(input, 'selective_dynamics'), 'w+') as f:
         f.write(str(sd_orig))
     neigh = poscar.structure.get_neighbors(poscar.structure.sites[atom], unfrozen_dist, True) # find nearby atoms
     neigh = list(map(lambda x: x[2], neigh)) # get indices
@@ -43,7 +43,7 @@ def freeze_atoms_except_neighbors(dir : str, atom : int, invert=False, unfrozen_
                 sd_orig[i] = [False, False, False]
     poscar.selective_dynamics = sd_orig
     Poscar.get_string = get_string_more_sigfig
-    poscar.write_file(os.path.join(dir, 'POSCAR'))
+    poscar.write_file(output)
     return
 
 def unfreeze_atoms(directory, sd_file='.selective_dynamics'):
@@ -73,9 +73,11 @@ if __name__ == '__main__':
     parser.add_argument('atom', help='atom number (1 indexed) to unfreeze around',
                         default=None, nargs="?", type=int)
     parser.add_argument('-r', '--radius', help='radius to freeze around (default 4)',
-                        type=float)
-    parser.add_argument('-d', '--directory', help='Input directory (default = ".")',
-                        default='.')
+                        type=float, default=4)
+    parser.add_argument('--input', help='input POSCAR file (default = "POSCAR")',
+                        default='POSCAR')
+    parser.add_argument('--output', help='output POSCAR file (default = "POSCAR.sd")',
+                        default='POSCAR.sd')
     parser.add_argument('-u', '--undo', help='Read selective_dynamics file and undo frozen atoms',
                         action='store_true')
     parser.add_argument('-i', '--invert', help='freeze atoms around index, instead of leaving radius available to move',
@@ -86,4 +88,4 @@ if __name__ == '__main__':
     if args.undo:
         unfreeze_atoms(args.directory, sd_file=args.sd)
     else:
-        freeze_atoms_except_neighbors(args.directory, args.atom, args.invert, args.radius)
+        freeze_atoms_except_neighbors(args.input, args.output, args.atom, args.invert, args.radius)
