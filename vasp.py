5372ce470fc2e7bfa5e5a458b777e63149728ecd
Ryan Trottier <themeisme@gmail.com>
2018-06-21 08:31:43 -0600

    set kpoints in right splot

--- vasp.py
+++ vasp.py
@@ -27,7 +27,7 @@ def get_instructions_for_backup(jobtype, incar='INCAR'):
 
     '''
     instructions = {}
-    instructions["commands"] = ['rm *.sh *.log *.out *.err STOPCAR *.e[0-9][0-9][0-9]* *.o[1-9][1-9][1-9]* *.log* &> /dev/null']
+    instructions["commands"] = ['rm *.sh *.out *.err STOPCAR *.e[0-9][0-9][0-9]* *.o[1-9][1-9][1-9]* &> /dev/null']
     instructions['backup'] = []
     instructions['move'] = []
     if jobtype == 'Standard':
