1e74a90e4c9e7e60e84f3b0508c58dde6f77d59d
Ryan Trottier <themeisme@gmail.com>
2018-07-13 14:20:08 -0600

    dont print

--- GSM_Setup.py
+++ GSM_Setup.py
@@ -184,8 +184,6 @@ def GSM_Setup(start, final=None, new_gsm_dir='.', images=None, center=[0.5,0.5,0
                             final_coord_diff = np.matrix([x,y,z]) * cell
                             distance_temp = np.linalg.norm(start_coord - (final_coord+final_coord_diff))
                             if distance_temp < distance:
-                                print(start_i+i)
-                                print('{} {} {}'.format(x,y,z))
                                 final_coord_temp = final_coord+final_coord_diff
                                 distance = distance_temp
                 lines[final_i+i][1] = final_coord_temp[0,0]
